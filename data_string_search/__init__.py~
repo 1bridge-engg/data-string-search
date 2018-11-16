a62df3ad68cef89dc8f771ae6102ec73e6eac0e1
saurabh <saurabhrane.m@gmail.com>
2018-11-16 17:29:48 +0530

    data_string_search_setup

---
+++ data_string_search/__init__.py~
@@ -0,0 +1,2 @@
+name = "py_advanced_search_list_prod"
+from .function import search
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data_string_search",
+    version="0.0.1",
+    author="Saurabh",
+    author_email="saurabhrane.m@gmail.com",
+    description="Package to advance search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saurabhranem/python_library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'fuzzywuzzy',
+        'fuzzywuzzy[speedup]',
+    ]
+)
